1bbed1a5548fd162f4d75ec91f4dedc41b46f891
rawatkajal7093-a11y <rawatkajal7093@gmail.com>
2026-06-04 17:10:40 +0530

    add rock paper scissor python code

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,11 @@
+import random
+
+user_score = 0
+computer_score = 0
+
+choices = ["rock", "paper", "scissors"]
+print("   ROCK PAPER SCISSORS GAME")
+
+while True:
+    print("\nChoose: Rock, Paper, or Scissors")
+    user_choice = input("Your choice: ").lower()
