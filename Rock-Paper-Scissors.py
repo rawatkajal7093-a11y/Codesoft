47ca89a15673f16d1b3b160a1fa1a01da82ded1b
rawatkajal7093-a11y <rawatkajal7093@gmail.com>
2026-06-04 17:23:55 +0530

    add python code

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -4,8 +4,26 @@ user_score = 0
 computer_score = 0
 
 choices = ["rock", "paper", "scissors"]
-print("   ROCK PAPER SCISSORS GAME")
+print("ROCK PAPER SCISSORS GAME")
 
 while True:
-    print("\nChoose: Rock, Paper, or Scissors")
+    print("Choose: Rock, Paper, or Scissors")
     user_choice = input("Your choice: ").lower()
+    
+    if user_choice not in choices:
+        print("Invalid choice! Please enter rock, paper, or scissors.")
+        continue
+    computer_choice = random.choice(choices)
+
+    print(f"You chose: {user_choice}")
+    print(f"Computer chose: {computer_choice}")
+
+    if user_choice == computer_choice:
+        print("It's a Tie!")
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "paper" and computer_choice == "rock") or
+        (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        print("You Win!")
+        user_score += 1    
